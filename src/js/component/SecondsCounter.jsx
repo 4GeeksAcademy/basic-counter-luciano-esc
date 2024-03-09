5b925dad7ed5ddc283e09066eee2336f329e8b5d
luciano-esc <149176025+luciano-esc@users.noreply.github.com>
2024-03-09 07:23:43 +0000

    finished

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+
+
+const SecondsCounter = (props) => {
+  let first = Math.floor(props.seconds / 1) % 10;
+  let second = Math.floor(props.seconds / 10) % 10;
+  let third = Math.floor(props.seconds / 100) % 10;
+  let fourth = Math.floor(props.seconds / 1000) % 10;
+  let fifth = Math.floor(props.seconds / 10000) % 10;
+  let sixth = Math.floor(props.seconds / 100000) % 10;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  return (
+    <div>
+      <div className="bg-black d-flex justify-content-center text-center mt-3 p-5">
+        <div
+          className="bg-black text-black mx-2 border border-5 border-danger"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          <span className="text-danger"><i className="far fa-clock"></i></span>
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {sixth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {fifth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {fourth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {third}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {second}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {first}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondsCounter;
